8de0d25e6930f00bb0e6ec3bfab145b044e9ad25
ibrahimgb <ibrahim.guoual.b@gmail.com>
2024-08-31 23:48:01 +0200

    test using all the sample data

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import unittest
 import json
 from flask import Flask, request, jsonify
 
-# Assuming your Flask app is defined here or imported from another module
+# Define the Flask app
 app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
@@ -18,31 +18,31 @@ class FlaskAppTestCase(unittest.TestCase):
         # Set up the Flask test client
         self.app = app.test_client()
         self.app.testing = True
-        
-    
+
+        # Load test data from a JSON file
+        with open('sampled_data.json', 'r') as file:
+            self.test_data = json.load(file)
 
     def test_predict(self):
-        # Define the payload
-        payload = {
-        "Air temperature [K]": 303.3,
-        "Process temperature [K]": 311.6,
-        "Rotational speed [rpm]": 1337,
-        "Torque [Nm]": 56.8,
-        "Tool wear [min]": 187,
-        "Type__H": 0,
-        "Type__L": 1,
-        "Type__M": 0
-    }
+        for data in self.test_data:
+            with self.subTest(data=data):
+                # Send a POST request to the /predict endpoint
+                response = self.app.post('/predict',
+                                        data=json.dumps(data),
+                                        content_type='application/json')
 
-        # Send a POST request to the /predict endpoint
-        response = self.app.post('/predict',
-                                data=json.dumps(payload),
-                                content_type='application/json')
+                # Check the response
+                self.assertEqual(response.status_code, 200)
+                # Use dictionary key access to retrieve the 'Target' value
+                target = data.get('Target')
 
-        # Check the response
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"result": "defect"})
-
+                if target == 0:
+                    self.assertEqual(response.json, {"result": "defect"})
+                elif target == 1:
+                    self.assertEqual(response.json, {"result": "defect"})
+                else:
+                    self.fail(f"Unexpected Target value: {target}")
+                
 
 if __name__ == '__main__':
     unittest.main()
